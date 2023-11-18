ab08932ce186320aa02c956f323c45193e2b8722
Max <lhmax55@gmail.com>
2023-11-18 16:02:23 -0500

    added a tilemap and collision

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const SPEED = 300
 
 func _ready():
 	velocity.x = 0 #SPEED/2
@@ -13,9 +9,5 @@ func _ready():
 
 func _physics_process(delta):
 	var direction = Input.get_axis("move_up","move_down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
-	move_and_slide()
+	move_and_collide(Vector2(0, direction * SPEED * delta))
+	print(delta)
